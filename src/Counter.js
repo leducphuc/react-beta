ea2cf3a27cca35ce01c8a0bc5c2c26e7e6c67e4b
Le Duc Phuc <leducphuc@MacBook-Pros.local>
2018-11-04 21:00:17 +0700

    prevent rendering

--- src/Counter.js
+++ src/Counter.js
@@ -14,29 +14,58 @@ const INITIAL_LIST = [
   }
 ];
 
-function makeData() {
-    let list = [];
-    for (let index = 0; index < 10000; index++) {
-        const newItem = {
-            id: index,
-            title: `111111111111111111113444444444444444444442222222 ${index}`,
-            url: `3333333333333333333333333333333333333333333333333333 ${index}`,
-            value: `888888888888lllllllllllllllllllllllllll ${index}`,
-            description: `dessssssssssssssssccccccccccccssssssss ${index}`,
-        }
-        list = [...list, newItem]
+const makeData = () => {
+  let list = [];
+  console.log('make data');
+  for (let index = 0; index < 10000; index++) {
+    const newItem = {
+      id: index,
+      title: `111111111111111111113444444444444444444442222222 ${index}`,
+      url: `3333333333333333333333333333333333333333333333333333 ${index}`,
+      value: `888888888888lllllllllllllllllllllllllll ${index}`,
+      description: `dessssssssssssssssccccccccccccssssssss ${index}`
+    };
+    list = [...list, newItem];
+  }
+  return list;
+};
+
+const INITIAL_DATA = makeData();
+
+// const ListItem = React.memo(function ListCounter(props) {
+//   const { item, index, removeItem } = props;
+//   console.log('item', index);
+//   return (
+//   );
+// });
+
+class ListItem extends React.Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        const { item, index, removeItem } = this.props;
+        if (nextProps.index !== index || item.id !== nextProps.item.id) return true;
+        return false;
     }
-    return list;
+    render() { 
+        console.log('render item');
+        const { item, index, removeItem } = this.props;
+        return (
+            <li key={item.id}>
+            <a href={item.url}>{item.title}</a>
+            <button onClick={() => removeItem(index)}>remove {index}</button>
+            </li>
+        )
+  }
 }
 
 const Counter = React.memo(function Counter() {
-  const [limit, setLimit] = useState(5);
+  const [limit, setLimit] = useState(100);
   const [offset, setOfflset] = useState(1);
-  const [list, setList] = useState(listSlice(limit));
+  const [list, setList] = useState([]);
   const [inputValue, setInput] = useState("");
 
   function listSlice(limit) {
-    const newList = [...makeData()];
+    console.log("list lsice");
+    const newList = [...INITIAL_DATA];
     return newList.slice(0, limit);
   }
 
@@ -47,32 +76,36 @@ const Counter = React.memo(function Counter() {
   }
 
   useEffect(() => {
-      console.log('pre-render');
+    console.log("pre-render");
   });
 
-  useEffect(() => {
-    setList(listSlice(limit));
-  }, [limit]);
+  useEffect(
+    () => {
+      console.log("list slice");
+      setList(listSlice(limit));
+    },
+    [limit]
+  );
 
   function addItem() {
     if (inputValue) {
-        const lastValue = list.length ? list[list.length -1] : 0;
-        const newValue =  () => {
-            if (lastValue) {
-              return {
-                  id: lastValue.id +1,
-                  title: inputValue,
-                  url: inputValue,
-              }
-            }
-            return {
-                id: 0,
-                title: inputValue,
-                url: inputValue,
-            }
+      const lastValue = list.length ? list[list.length - 1] : 0;
+      const newValue = () => {
+        if (lastValue) {
+          return {
+            id: lastValue.id + 1,
+            title: inputValue,
+            url: inputValue
+          };
         }
-        setList([...list, newValue()]);
-        setInput('');
+        return {
+          id: 0,
+          title: inputValue,
+          url: inputValue
+        };
+      };
+      setList([...list, newValue()]);
+      setInput("");
     }
   }
 
@@ -80,12 +113,7 @@ const Counter = React.memo(function Counter() {
     <div>
       <ul>
         {list.map((item, index) => {
-            return (
-                <li key={item.id}>
-                  <a href={item.url}>{item.title}</a>
-                  <button onClick={() => removeItem(index)}>remove {index}</button>
-                </li>
-              )
+          return <ListItem key={item.id} item={item} index={index} removeItem={removeItem}/>;
         })}
       </ul>
       <span>
@@ -96,9 +124,7 @@ const Counter = React.memo(function Counter() {
         />
         <button onClick={addItem}>Add Item</button>
       </span>
-      <button onClick={() => setLimit(limit + 500)}>
-            Load More
-      </button>
+      <button onClick={() => setLimit(limit + 500)}>Load More</button>
     </div>
   );
 });
